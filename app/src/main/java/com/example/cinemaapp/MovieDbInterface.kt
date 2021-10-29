88d15bafcaf5734722f3afadbd427ba21e5857a3
vipulasri <vipulasri.2007@gmail.com>
2021-10-30 00:25:27 +0530

    update: popular movie api path

--- app/src/main/java/com/example/cinemaapp/MovieDbInterface.kt
+++ app/src/main/java/com/example/cinemaapp/MovieDbInterface.kt
@@ -12,7 +12,7 @@ interface MovieDbInterface {
     //https://api.themoviedb.org/3/movie/upcoming?api_key=
     //https://api.themoviedb.org/3/
 
-    @GET("/movie/popular")
+    @GET("movie/popular")
     fun getMovieDetails(
         @Query("api_key") apiKey: String = "39c7fb0822b42e228245de1f7c8d5a48"
     ):Call<MovieDetails>
